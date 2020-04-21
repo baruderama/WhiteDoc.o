f27627f842ed243a563f90e1830d84e62dcc6ef8
Sir Sebas <sebasvalderrama240@outlook.com>
2020-04-21 06:06:38 -0500

    lista de usuarios de la app+ pantalla de lista de chats

--- Aplicacion/app/src/main/java/e/asus/whitedoc/Chat.java
+++ Aplicacion/app/src/main/java/e/asus/whitedoc/Chat.java
@@ -1,16 +1,111 @@
 package e.asus.whitedoc;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import Fragments.ChatsFragment;
+import Fragments.UsersFragment;
+import de.hdodenhof.circleimageview.CircleImageView;
+import model.User;
 
 public class Chat extends AppCompatActivity {
+
+    CircleImageView profile_image;
+    TextView username;
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        profile_image=findViewById(R.id.profile_image);
+        username=findViewById(R.id.username);
 
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+        reference= FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
 
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user=dataSnapshot.getValue(User.class);
+                username.setText(user.getName());
+                //if(user.getImageURL().equals("default")){
+                    profile_image.setImageResource(R.mipmap.ic_launcher);
+                //} else {
+                  //  Glide.with(Chat.this).load(user.getImageURL()).into(profile_image);
+
+                //}
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        TabLayout tabLayout= findViewById(R.id.tab_layout);
+        ViewPager viewPager= findViewById(R.id.view_pager);
+
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new ChatsFragment(),"Chats");
+        viewPagerAdapter.addFragment(new UsersFragment(),"Users");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPagerAdapter(FragmentManager fm){
+            super(fm);
+            this.fragments=new ArrayList<>();
+            this.titles=new ArrayList<>();
+
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+        public void addFragment(Fragment fragment,String title){
+            fragments.add(fragment);
+            titles.add(title);
+
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
     }
 }
--- Aplicacion/app/src/main/java/e/asus/whitedoc/ui/login/LoginActivity.java
+++ Aplicacion/app/src/main/java/e/asus/whitedoc/ui/login/LoginActivity.java
@@ -23,6 +23,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import e.asus.whitedoc.Chat;
 import e.asus.whitedoc.PantallaPrincipalMedico;
 import e.asus.whitedoc.PantallaPrincipalUsuario;
 import e.asus.whitedoc.R;
@@ -119,7 +120,7 @@ public class LoginActivity extends AppCompatActivity {
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
 
-                    Intent pantallaPrincipal = new Intent(getApplicationContext(), PantallaPrincipalUsuario.class);
+                    Intent pantallaPrincipal = new Intent(getApplicationContext(), Chat.class);
                     startActivity(pantallaPrincipal);
 
             }
