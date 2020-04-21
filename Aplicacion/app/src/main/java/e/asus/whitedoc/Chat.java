94f84131652708ce5e090c1249805c13a222f7c1
Sir Sebas <sebasvalderrama240@outlook.com>
2020-04-21 01:34:08 -0500

    prueba2

--- Aplicacion/app/src/main/java/e/asus/whitedoc/Chat.java
+++ Aplicacion/app/src/main/java/e/asus/whitedoc/Chat.java
@@ -10,5 +10,7 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+
     }
 }
